7718eba920cd1ed1f7a7aeb20f944428785f5d27
Raghu K <raghuk.vit@gmail.com>
2026-05-04 07:04:57 +0530

    RoboShop Code

--- 02-create-vm-with-dns/main.tf
+++ 02-create-vm-with-dns/main.tf
@@ -23,8 +23,6 @@ resource "azurerm_linux_virtual_machine" "main" {
   network_interface_ids = [azurerm_network_interface.main.id]
   size               = "Standard_D2s_v3"
 
-  delete_os_disk_on_termination = true
-
   source_image_id = "/subscriptions/3f2e42e1-ca06-4a99-8c56-be8d8ba306db/resourceGroups/denmark-east-rg/providers/Microsoft.Compute/galleries/rhel10/images/1.0.0/versions/1.0.0"
 
   os_disk {
