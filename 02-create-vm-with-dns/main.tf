ee2b379bdeddb89abbae6ebb6e9b589aa7ecf4a4
Raghu K <raghuk.vit@gmail.com>
2026-05-04 07:04:07 +0530

    RoboShop Code

--- 02-create-vm-with-dns/main.tf
+++ 02-create-vm-with-dns/main.tf
@@ -21,30 +21,21 @@ resource "azurerm_linux_virtual_machine" "main" {
   location              = "Denmark East"
   resource_group_name   = "denmark-east-rg"
   network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_D2s_v3"
+  size               = "Standard_D2s_v3"
 
   delete_os_disk_on_termination = true
 
-  storage_image_reference {
-    id = "/subscriptions/3f2e42e1-ca06-4a99-8c56-be8d8ba306db/resourceGroups/denmark-east-rg/providers/Microsoft.Compute/galleries/rhel10/images/1.0.0/versions/1.0.0"
+  source_image_id = "/subscriptions/3f2e42e1-ca06-4a99-8c56-be8d8ba306db/resourceGroups/denmark-east-rg/providers/Microsoft.Compute/galleries/rhel10/images/1.0.0/versions/1.0.0"
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
 
-  storage_os_disk {
-    name              = "test-vm"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
+  admin_password = "DevOps@123456"
+  admin_username = "devops"
 
-  os_profile {
-    computer_name  = "test-vm"
-    admin_username = "devops"
-    admin_password = "DevOps@123456"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
+  disable_password_authentication = false
 
   secure_boot_enabled = true
   vtpm_enabled        = true
