cb4c97339073fc2cd6cdb1a23cfa559e3d1778db
Raghu K <raghuk.vit@gmail.com>
2026-05-05 06:36:25 +0530

    RoboShop Code

--- 04-count/main.tf
+++ 04-count/main.tf
@@ -1,3 +1,25 @@
 resource "null_resource" "a" {
   count = 10
 }
+
+provider "azurerm" {
+  features {}
+}
+
+variable "components" {
+  default = ["frontend", "mysql", "catalogue"]
+}
+
+resource "azurerm_network_interface" "private-ips" {
+  count = 3
+  name                = "${var.components[count.index]}-nic"
+  location            = "Denmark East"
+  resource_group_name = "denmark-east-rg"
+
+  ip_configuration {
+    name                          =  "${var.components[count.index]}-nic"
+    subnet_id                     = "/subscriptions/3f2e42e1-ca06-4a99-8c56-be8d8ba306db/resourceGroups/denmark-east-rg/providers/Microsoft.Network/virtualNetworks/rhel10-vmVNET/subnets/rhel10-vmSubnet"
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
