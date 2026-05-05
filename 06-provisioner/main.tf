da9001cdde4173f19cba3015b945dc5f775b6fae
Raghu K <raghuk.vit@gmail.com>
2026-05-05 07:08:03 +0530

    RoboShop Code

---
+++ 06-provisioner/main.tf
@@ -0,0 +1,44 @@
+resource "azurerm_network_interface" "frontend" {
+  name                = "frontend-nic"
+  location            = "Denmark East"
+  resource_group_name = "denmark-east-rg"
+
+  ip_configuration {
+    name                          =  "frontend-nic"
+    subnet_id                     = "/subscriptions/3f2e42e1-ca06-4a99-8c56-be8d8ba306db/resourceGroups/denmark-east-rg/providers/Microsoft.Network/virtualNetworks/rhel10-vmVNET/subnets/rhel10-vmSubnet"
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.frontend.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "frontend" {
+  name                  = "frontend-vm"
+  location              = "Denmark East"
+  resource_group_name   = "denmark-east-rg"
+  network_interface_ids = [azurerm_network_interface.frontend.id]
+  size               = "Standard_B1s"
+
+  source_image_id = "/subscriptions/3f2e42e1-ca06-4a99-8c56-be8d8ba306db/resourceGroups/denmark-east-rg/providers/Microsoft.Compute/galleries/rhel10/images/1.0.0/versions/1.0.0"
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  admin_password = "DevOps@123456"
+  admin_username = "devops"
+
+  disable_password_authentication = false
+
+  secure_boot_enabled = true
+  vtpm_enabled        = true
+
+}
+
+resource "azurerm_dns_a_record" "frontend" {
+  name                = "frontend-dev"
+  zone_name           = "rdevopsb89.online"
+  resource_group_name = "denmark-east-rg"
+  ttl                 = 30
+  records             = [azurerm_network_interface.frontend.private_ip_address]
+}
