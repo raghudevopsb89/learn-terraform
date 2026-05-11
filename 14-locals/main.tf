1c2d8296a1aae33097e03f20b94d8f6773f77378
Raghu K <raghuk.vit@gmail.com>
2026-05-11 07:23:41 +0530

    RoboShop Code

--- 14-locals/main.tf
+++ 14-locals/main.tf
@@ -6,3 +6,26 @@ output "a" {
   value = local.a
 }
 
+resource "azurerm_public_ip" "frontend" {
+  name                = "frontend"
+  location            = "Denmark East"
+  resource_group_name = "denmark-east-rg"
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "frontend" {
+  name                = "frontend-nic"
+  location            = "Denmark East"
+  resource_group_name = "denmark-east-rg"
+
+  ip_configuration {
+    name                          =  "frontend-nic"
+    subnet_id                     = "/subscriptions/3f2e42e1-ca06-4a99-8c56-be8d8ba306db/resourceGroups/denmark-east-rg/providers/Microsoft.Network/virtualNetworks/rhel10-vmVNET/subnets/rhel10-vmSubnet"
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.frontend.id
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
